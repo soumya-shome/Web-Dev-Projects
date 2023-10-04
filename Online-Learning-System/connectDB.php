7ce09e8cc8058f53387e5abc7f98ad3fb50ef217
Soumyadeep Shome <soumyadeepshome99@gmail.com>
2023-10-04 17:08:16 +0530

    Updated

--- Online-Learning-System/connectDB.php
+++ Online-Learning-System/connectDB.php
@@ -1,9 +1,11 @@
-<?php 
-	$con =  new mysqli('localhost','root','');
+<?php
+	$db_name="attendance_log";
+	$db_user="root";
+	$db_password="";
+	$con =  new mysqli('localhost',$db_user,$db_password);
 	global $con;
 	if(!$con){
 		die ("Couldn't connect ".mysql_error());
 	}
-	mysqli_select_db($con,"register");
-	mysqli_query($con,"CREATE TABLE IF NOT EXISTS `studentlog`(`fname` varchar(40) NOT NULL,`lname` varchar(40) NOT NULL,`gender` char(1) NOT NULL,`email` varchar(45) NOT NULL,`password` varchar(20) NOT NULL,`date` date NOT NULL,`course` varchar(30) NOT NULL,`phone1` int(40) NOT NULL,`phone2` int(40) NOT NULL,`cardid` int(30) NOT NULL,PRIMARY KEY (`cardid`)) ENGINE=MyISAM DEFAULT CHARSET=latin1;");
+	mysqli_select_db($con,$db_name);
 ?>
