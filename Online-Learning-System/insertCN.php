2aca0697c1b5afd8c2452433afab032d36087108
Soumyadeep Shome <soumyadeepshome99@gmail.com>
2024-01-15 17:54:37 +0530

    15-01-24

--- Online-Learning-System/insertCN.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(isset($_REQUEST['b1'])){
-	require("connectDB.php");
-	mysqli_query($con,"DROP TABLE IF EXISTS `tempcd`;");
-	mysqli_query($con,"CREATE TABLE IF NOT EXISTS `tempcd` (`CardNo` varchar(50) NOT NULL);");
-	$card=$_REQUEST['cd'];
-	$result = mysqli_query($con,"INSERT INTO `tempcd` (`CardNo`) VALUES ('".$card."');");
-}
-?>
